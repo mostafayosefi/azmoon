419876499fbbdca9a295c1b7c3aeacdf917c6963
unknown <mustafa1390@gmail.com>
2024-01-05 20:26:48 +0330

    set getway payping v1

--- app/Http/Controllers/user/UserController.php
+++ app/Http/Controllers/user/UserController.php
@@ -2853,13 +2853,21 @@ $nametrt = Session::flash('sessurl', 'viewsonlineshops');
 
 
 
-//  start zarinpal
+$setting = DB::table('setting')->where('id' , 1)->orderBy('id', 'desc')->orderBy('id', 'desc')->first();
+$getway_payment = $setting->getway_payment;
+if($myuser->user_email=='mustafa1390@gmail.com'){
+    $price='5060';
+    $getway_payment = 'payping';
+}
 
+
+    if($getway_payment=='zarinpal'){
+        
+//  start zarinpal
 $myuser = DB::table('user')->where([
     ['id',  Session::get('iduser')],
 ])->first();
  
-if($myuser->user_email=='mustafa1390@gmail.com'){$price='5060';}
 
 $data = array("merchant_id" => "f373affa-e1bd-11e8-bcb5-005056a205be",
 "amount" => $price,
@@ -2905,6 +2913,140 @@ if (empty($result['errors'])) {
 
 
 //  end zarinpal
+
+    }
+
+    if($getway_payment=='payping'){
+
+         
+                    $token = "PJ__XCI8AR-pL5c4GCOQc3auTQzk2wPPKJ7hgYKq3U0";
+                    $args = array(
+                        "amount" => $price,
+                        "payerIdentity" => $myuser->user_email,
+                        "payerName" => $myuser->user_name ,
+                        "description" => $myuser->user_tell ,
+                        "returnUrl" => 'https://azmoonpte.com/servicepay/user/callback/payping/' . $myrequest->req_rnd,
+                        "clientRefId" => "$myrequest->req_rnd"
+                    );
+
+                    try {
+                        $curl = curl_init();
+                        curl_setopt_array($curl, array(
+                                CURLOPT_URL => "https://api.payping.ir/v2/pay",
+                                CURLOPT_RETURNTRANSFER => true,
+                                CURLOPT_POSTFIELDS => json_encode($args),
+                                CURLOPT_HTTPHEADER => array(
+                                    "accept: application/json",
+                                    "authorization: Bearer " . $token,
+                                    "cache-control: no-cache",
+                                    "content-type: application/json"
+                                ),
+                            )
+                        );
+                        $result = curl_exec($curl);
+                        curl_close($curl);
+                        $transaction_info = json_decode($result, true);
+                        // dd($result);
+                        if ($transaction_info['code']) {
+                            // $order->transactionId = $transaction_info['code'];
+                            // $order->save();
+                            // session_start();
+                            if (isset($_SESSION['refresh2'])) unset($_SESSION['refresh2']);
+                            if (isset($_SESSION['refresh'])) unset($_SESSION['refresh']);
+                            return redirect('https://api.payping.ir/v2/pay/gotoipg/' . $transaction_info['code']);
+                        } else throw new \Exception("مشکلی در پرداخت وجود دارد");
+
+                    } catch (\Exception $e) {
+                        echo $e->getMessage();
+
+                    }
+
+
+    }
+
+
+
+
+  			}
+else if (Session::get('activeuser')==0){    return redirect('user/activition'); }
+else if (empty(Session::has('signuser'))){   return redirect('user/sign-in'); }
+}
+	}
+
+
+
+    
+public function callback_payping ($id , Request $request ){
+	if (Session::has('signuser')){
+		if (Session::get('activeuser')==1){
+
+
+            
+            
+$req_rnd = $id;
+$myrequest = \DB::table('myrequest')
+->where([
+    ['myrequest.req_rnd', '=', $req_rnd],
+    ['myrequest.req_userid', '=', Session::get('iduser')], ])
+    ->orderBy('myrequest.req_id', 'desc')->first();
+
+$myuser = DB::table('user')->where([
+    ['id',  Session::get('iduser')],
+])->first();
+
+$price = $myrequest->req_price;
+
+if($myuser->user_email=='mustafa1390@gmail.com'){$price='5060';}
+
+
+
+
+
+
+                $refid; 
+
+                if (isset($_POST['refid'])) $refid = $_POST['refid'];
+                else $refid = 0;
+ 
+  
+                $token = "PJ__XCI8AR-pL5c4GCOQc3auTQzk2wPPKJ7hgYKq3U0"; 
+
+                $args = array(
+                    'amount' => $price,
+                    'refId' => $refid
+                );
+                try {
+                    $curl = curl_init();
+                    curl_setopt_array($curl, array(
+                            CURLOPT_URL => "https://api.payping.ir/v2/pay/verify",
+                            CURLOPT_RETURNTRANSFER => true,
+                            CURLOPT_POSTFIELDS => json_encode($args),
+                            CURLOPT_HTTPHEADER => array(
+                                "accept: application/json",
+                                "authorization: Bearer " . $token,
+                                "cache-control: no-cache",
+                                "content-type: application/json"
+                            ),
+                        )
+                    );
+                    $result = curl_exec($curl);
+                    $header = curl_getinfo($curl);
+                    curl_close($curl);
+                    $callback_info = json_decode($result, true);
+
+                    dd($callback_info);
+
+                    if ($callback_info['cardNumber']) { }
+
+
+
+
+
+    // return  redirect('user/error/'.$req_rnd);
+    // return  redirect('user/success/'.$req_rnd);
+
+
+
 
 
 
